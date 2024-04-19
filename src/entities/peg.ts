82e64d58858e4a74bf9efc69d481d8ff9cd27c06
Ryan Langman <ryan.m.langman@gmail.com>
2024-04-19 18:33:57 +0200

    Peg layout and debugging settings
    - Lays out pegs, vertically and horizontally centered in the canvas,
    according to the number of specified levels.
    - Renders the pegs out to the canvas with PixiJS's render loop.
    - Adds a launch.json file to enable debugging. First run with
    parcel then launch a debugger in VSCode to attach debugger in chrome.

--- src/entities/peg.ts
+++ src/entities/peg.ts
@@ -2,9 +2,15 @@ import * as PIXI from 'pixi.js';
 
 export class Peg {
     private graphic: PIXI.Graphics;
+    private colour: string = "red";
 
-    constructor() {
-
+    constructor(x, y, radius) {
+        this.graphic = new PIXI.Graphics();
+        this.graphic.circle(x, y, radius);
+        this.graphic.fill(this.colour);
     }
 
+    render(container: PIXI.Container) {
+        container.addChild(this.graphic)
+    }
 }
