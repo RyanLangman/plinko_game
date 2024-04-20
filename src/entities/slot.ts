1a11ac64519a5008990a4b41b582efdf4b0c9a69
Ryan Langman <ryan.m.langman@gmail.com>
2024-04-20 12:03:10 +0200

    Adds slots beneath last row of pegs
    - Introduces some slots beneath the last row of pegs.
    - Updates slot entity to render a rectangle with a yellow colour and
    adds red text within the block.

--- src/entities/slot.ts
+++ src/entities/slot.ts
@@ -2,8 +2,30 @@ import * as PIXI from 'pixi.js';
 
 export class Slot {
     private graphic: PIXI.Graphics;
+    private colour: string = "yellow";
+    private text: PIXI.Text;
 
-    constructor() {
+    constructor(x: number, y: number, width: number, height: number, text: string) {
+        this.graphic = new PIXI.Graphics();
+        this.graphic.rect(x, y, width, height);
+        this.graphic.fill(this.colour);
 
+        this.text = new PIXI.Text({
+            text: text,
+            style: {
+                fontFamily: 'Arial',
+                fontSize: 12,
+                fill: 0xff1010,
+                align: 'center',
+            }
+        })
+
+        this.text.x = x + 5;
+        this.text.y = y + 5;
+    }
+
+    render(container: PIXI.Container) {
+        container.addChild(this.graphic);
+        container.addChild(this.text);
     }
 }
