5af29ad9901ee4a507ec52353773dad60249d8f6
Ryan Langman <ryan.m.langman@gmail.com>
2024-04-20 16:21:35 +0200

    Coordinate for ball position
    - Fixed peg count for odd rows on pegboard.

--- src/entities/board.ts
+++ src/entities/board.ts
@@ -3,20 +3,22 @@ import { Peg } from './peg';
 import { Ball } from './ball';
 import { Slot } from './slot';
 
+type Coordinate = [number, number];
+
 export class Board {
     private slots: Slot[];
     private pegs: Peg[][];
     private pegRadius: number = 5;
     private pegDiameter: number = this.pegRadius * 2;
     private ball: Ball;
+    private ballPosition: Coordinate = [0, 2]; // Always starts above middle peg, first row
 
     constructor(canvasWidth: number, canvasHeight: number, levels: number = 3) {
         // TODO: Refactor to own method
-        const pegsPerRow = 5;
+        const maxPegsPerRow = 5;
         this.pegs = [];
         this.slots = [];
 
-        // TODO: Even rows should have 1 less peg
         for (let x = 0; x < levels; x++) {
             this.pegs[x] = [];
         }
@@ -27,11 +29,14 @@ export class Board {
         let yOffset = 60;
         const verSpaceBetweenPegs = 30 + this.pegDiameter;
     
+        let pegsPerRow = maxPegsPerRow;
         for (let i = 0; i < levels; i++) {
             if (i % 2 === 0) {
                 xOffset = 40;
+                pegsPerRow = maxPegsPerRow;
             } else {
                 xOffset = oddRowOffset;
+                pegsPerRow = maxPegsPerRow - 1;
             }
 
             let xPosition = xOffset;
@@ -46,7 +51,7 @@ export class Board {
         }
 
         // TODO: Refactor to own method
-        const totalSlots = pegsPerRow - 1
+        const totalSlots = maxPegsPerRow - 1
         const slotWidth = 39;
         const slotHeight = 30;
         yOffset = (yOffset + this.pegDiameter) - verSpaceBetweenPegs;
@@ -62,7 +67,8 @@ export class Board {
     }
 
     dropBall() {
-
+        console.log('Dropping ball.');
+        
     }
 
     render(container: PIXI.Container) {
